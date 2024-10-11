39d98edf9bdadc4d5cfdbbb713a8173c5b26cc05
Lee <lsangm0513#gmail.com>
2024-10-11 16:41:57 +0900

    Your commit message

--- src/components/ChatWindow copy.jsx
+++ src/components/ChatWindow copy.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./ChatWindow copy.css";
+import "./ChatWindow.css";
 
 const ChatWindowCopy = ({ selectedUser, userId }) => {
   const [message, setMessage] = useState("");
@@ -39,39 +39,34 @@ const ChatWindowCopy = ({ selectedUser, userId }) => {
   };
 
   return (
-      <div className="chat-window">
-        <div className="chat-header">
-          <h2>{selectedUser} JOYTALK</h2>
-        </div>
-        <div className="chat-messages">
-          {/* 배경 비디오 추가 */}
-          <video
-              className="background-video"
-              autoPlay
-              loop
-              muted
-          >
-            <source src="/video/sunny.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          {messages.map((msg, index) => (
-              <div
-                  key={index}
-                  className={`message ${msg.from === userId ? "send" : "received"}`}
-              >
-                {msg.message}
-              </div>
-          ))}
-        </div>
-        <form className="chat-input" onSubmit={sendMessage}>
-          <input
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              placeholder="메시지 입력"
-          />
-          <button type="submit">전송</button>
-        </form>
+    <div className="chat-window">
+      <div className="chat-header">
+        <h2>{selectedUser} JOYTALK</h2>
       </div>
+      <div className="chat-messages">
+        {/* 배경 비디오 추가 */}
+        <video className="background-video" autoPlay loop muted>
+          <source src="/video/sunny.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        {messages.map((msg, index) => (
+          <div
+            key={index}
+            className={`message ${msg.from === userId ? "send" : "received"}`}
+          >
+            {msg.message}
+          </div>
+        ))}
+      </div>
+      <form className="chat-input" onSubmit={sendMessage}>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="메시지 입력"
+        />
+        <button type="submit">전송</button>
+      </form>
+    </div>
   );
 };
 
