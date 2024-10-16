d146007f623462ec7cc7dbdf127f861ab6760ff1
Lee <lsangm0513#gmail.com>
2024-10-16 14:04:37 +0900

    login css

--- src/components/Iphone.jsx
+++ src/components/Iphone.jsx
@@ -5,17 +5,17 @@ function Iphone({ children }) {
   return (
     <div className="phone-container">
       
-    <div className="volume-buttons"></div> 
-    <div className="power-button"></div> 
-    <div className="phone-edge">
-    <div className="phone">
-    <div className="phone-top">
-      <div className="phone-speaker"></div>
-      <div className="phone-camera"></div>
-      <div className="phone-lens"></div>
-    </div>
+     <div className="volume-buttons"></div> 
+     <div className="power-button"></div> 
+     <div className="phone-edge">
+      <div className="phone">
+       <div className="phone-top">
+        <div className="phone-speaker"></div>
+        <div className="phone-camera"></div>
+        <div className="phone-lens"></div>
+      </div>
       <div className="content">{children}</div> {/* 컨텐츠는 그대로 유지 */}
-    </div>
+     </div>
     </div>
   </div>
   );
--- src/router/LogIn.jsx
+++ src/router/LogIn.jsx
@@ -1,45 +1,38 @@
 import React from "react";
-import {Link} from "react-router-dom"; // Link import
-import {GoogleOAuthProvider, GoogleLogin} from '@react-oauth/google';
+import { Link } from "react-router-dom"; // Link import
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import "../css/LogIn.css";
 
 const LogIn = () => {
-    const handleGoogleLoginSuccess = (response) => {
-        console.log('Google login success', response);
-        // 여기서 토큰을 받아와 서버에 보내거나, 사용자의 정보를 처리할 수 있다.
-    };
+  const handleGoogleLoginSuccess = (response) => {
+    console.log("Google login success", response);
+    // 여기서 토큰을 받아와 서버에 보내거나, 사용자의 정보를 처리할 수 있다.
+  };
 
-    const handleGoogleLoginFailure = (error) => {
-        console.log('Google login failed', error);
-    };
+  const handleGoogleLoginFailure = (error) => {
+    console.log("Google login failed", error);
+  };
 
-
-    return (
-        <GoogleOAuthProvider clientId="496426685229-nni0d8e2ajcfee5dj9f42223mr963kel.apps.googleusercontent.com">
-            <div>
-                <h2>Log In Page</h2>
-                <form>
-                    <div>
-                        email:
-                        <input type="email" required/>
-                    </div>
-                    <div>
-                        pw:
-                        <input type="password" required/>
-                    </div>
-                    <Link to="/Profile">
-                        <button type="loginbutton">Log In</button>
-                    </Link>
-                </form>
-
-                {/*Google Login Button*/}
-                <GoogleLogin
-                    onSuccess={handleGoogleLoginSuccess}
-                    onError={handleGoogleLoginFailure}
-                />
-            </div>
-        </GoogleOAuthProvider>
-    );
+  return (
+    <GoogleOAuthProvider clientId="496426685229-nni0d8e2ajcfee5dj9f42223mr963kel.apps.googleusercontent.com">
+      <div className="login-background">
+        <div className="login-box">
+          Login
+          {/*Google Login Button*/}
+          <div className="login-google">
+            <GoogleLogin
+              onSuccess={handleGoogleLoginSuccess}
+              onError={handleGoogleLoginFailure}
+            />
+          </div>
+          {/*Kakao Login Button*/}
+          <Link to="/Profile">
+            <button type="loginbutton">카카오로그인</button>
+          </Link>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
 };
 
 export default LogIn;
